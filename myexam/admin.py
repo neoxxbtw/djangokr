b347f4fc75a97e6cc32f6576d101d9c5047c3fb8
neoxxbtw <neoxx04110@gmail..com>
2025-05-28 11:33:30 +0300

    Настройка админки

--- myexam/admin.py
+++ myexam/admin.py
@@ -4,20 +4,14 @@ from .models import ivexam
 
 
 @admin.register(ivexam)
-class ExamAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'exam_date', 'is_public')
-    list_filter = ('is_public', 'created_at')
+class ivexamAdmin(admin.ModelAdmin):
+
     search_fields = ('title', 'users__email')
+
     date_hierarchy = 'exam_date'
+
     filter_horizontal = ('users',)
 
-    def get_search_results(self, request, queryset, search_term):
-        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
-        if '@' in search_term:
-            queryset |= self.model.objects.filter(users__email=search_term)
-        return queryset, use_distinct
+    list_filter = ('is_public', 'created_at')
 
-
-from django.contrib import admin
-
-# Register your models here.
+    list_display = ('title', 'created_at', 'exam_date', 'is_public')
