19a2db0aebb7aa9959de7a6a626c7ee7ecc06c18
neoxxbtw <neoxx04110@gmail..com>
2025-05-28 11:05:42 +0300

    Создана модели и зарегистрирована в админке

--- mysite/myexam/admin.py
+++ mysite/myexam/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+@admin.register(ivexam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'exam_date', 'is_public')
+    list_filter = ('is_public', 'exam_date')
+    search_fields = ('title',)
 
-# Register your models here.
+admin.site.register(ivexam)
