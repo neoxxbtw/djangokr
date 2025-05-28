0ba163cc653e8f49c406218fea3bf8874534bd8d
neoxxbtw <neoxx04110@gmail..com>
2025-05-28 11:46:51 +0300

    Создание страницы (ФИО, группа)

--- myexam/views.py
+++ myexam/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import ivexam
+
+def exam_list(request):
+    exams = ivexam.objects.filter(is_public=True)
+    context = {
+        'exams': exams,
+        'title': 'ФИО: Венедиктов Иван Дмитриевич, Группа: 241-671'
+    }
+    return render(request, 'exam/exam_list.html', context)
