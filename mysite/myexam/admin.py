5e7e16e208c70d5104a06181b232f98739d5ab91
neoxxbtw <neoxx04110@gmail..com>
2025-05-28 11:12:53 +0300

    Создана модели и зарегистрирована в админке

--- mysite/myexam/admin.py
+++ mysite/myexam/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+
+from mysite.myexam.models import ivexam
+
+
 @admin.register(ivexam)
 class ExamAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at', 'exam_date', 'is_public')
-    list_filter = ('is_public', 'exam_date')
-    search_fields = ('title',)
+    list_filter = ('is_public', 'created_at')
+    search_fields = ('title', 'users__email')
+    date_hierarchy = 'exam_date'
+    filter_horizontal = ('users',)
 
-admin.site.register(ivexam)
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        if '@' in search_term:
+            queryset |= self.model.objects.filter(users__email=search_term)
+        return queryset, use_distinct
